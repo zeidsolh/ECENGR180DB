ac8cb13e314e257578e59683115fadd5a81ab5a5
zeidsolh <74027562+zeidsolh@users.noreply.github.com>
2023-04-16 16:37:34 -0700

    Added High Score

--- Music Ninja/Assets/Scripts/Game Logic/endScreen.cs
+++ Music Ninja/Assets/Scripts/Game Logic/endScreen.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
 
 namespace MirrorBasics
 {
@@ -14,12 +15,20 @@ namespace MirrorBasics
         int score;
         public TMP_Text scoreText;
         public int totalPossibleNotes;
+        Button continueButton;
+
+        public TMP_Text highScoreText;
 
         void Start()
         {
             inputObject = GameObject.Find("Hitbox Trigger Box");
             inputScript = inputObject.GetComponent<getInput>();
             score = inputScript.playerScore;
+            if(score > PlayerPrefs.GetInt("HighScore", 0))
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+            highScoreText.text = "High Score = " + PlayerPrefs.GetInt("HighScore", 0).ToString();
         }
 
         void Update()
@@ -38,6 +47,11 @@ namespace MirrorBasics
             percent = percent * 100;
             percent = Math.Truncate(percent);
             scoreText.text = "Player Name\n" + "Score " + score.ToString() + "\n" + percent.ToString() + "% Accuracy";
+        }
+
+        public void ContinueFunction()
+        {
+            SceneManager.LoadScene("MainMenu");
         }
 
     }
